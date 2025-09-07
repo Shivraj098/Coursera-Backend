d9328666e278d37a1e89e88bd1d9142079f8a2f6
Shivraj <rajshivraj5555@gmail.com>
2025-09-07 16:48:30 +0530

    Created endpoints or skeleton routes  for user

--- index.js
+++ index.js
@@ -2,7 +2,34 @@ const express = require('express');
 const app = express();
 const jwt = require("jsonwebtoken");
 
+app.post("/users/signup",function(req,res){
+    res.json({
+        message: "Sign up endpoint"
+    })
+})
 
+app.post("/users/signin",function(req,res){
+    res.json({
+        message: "Sign in endpoint"
+    })
+})
 
+app.get("/users/purchases",function(req,res){
+    res.json({
+        message: "All the courses that user purchased"
+    })
+})
+
+app.post("/course/purchase",function(req,res){
+    res.json({
+        message: "To purchase a course"
+    })
+})
+
+app.get("/courses",function(req,res){
+    res.json({
+        message: "All the courses that are available"
+    })
+})
 
 app.listen(3000);
